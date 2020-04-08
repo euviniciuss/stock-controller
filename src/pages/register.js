78a89196fa4b24f9f3cccc8a77b62a53bbd46baf
dev-viniciuss <dev.mvsc@gmail.com>
2020-04-08 12:51:32 -0300

    Layout da tela de cadastro de produtos

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,7 +1,78 @@
 import React from 'react';
+import { FiCheck, FiAlertTriangle, FiTrash } from 'react-icons/fi';
 
 export default function Register(){
     return(
-        <h1>Hello Register</h1>
+        <div className="card">
+            <div className="card-header">
+                <h4>Cadastro de Produtos</h4>
+            </div>
+
+            <div className="card-body">
+
+                <div className="row">
+                    <div className="col-md-6">
+                       <div className="form-group">
+                           <label><strong>Nome: </strong></label>
+                           <input type="text" className="form-control"/>
+                       </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="form-group">
+                           <label><strong>SKU: </strong></label>
+                           <input type="text" className="form-control"/>
+                       </div>
+                    </div>
+                </div>
+
+                <hr class="my-2" />
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <label><strong>Descrição: </strong></label>
+                        <textarea className="form-control" /> 
+                    </div>
+                </div>
+
+                <hr class="my-2" />
+
+                <div className="row">
+                    <div className="col-md-6">
+                       <div className="form-group">
+                           <label><strong>Preço: </strong></label>
+                           <input type="text" className="form-control"/>
+                       </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="form-group">
+                           <label><strong>Fornecedor: </strong></label>
+                           <input type="text" className="form-control"/>
+                       </div>
+                    </div>
+                </div>
+
+                <hr class="my-2" />
+
+                <div className="row">
+                    <div className="col-md-1">
+                        <button className="d-flex align-items-center btn btn-success" title="Salvar">
+                            <FiCheck />&nbsp;Salvar
+                        </button>
+                    </div>
+                    <div className="col-md-1">
+                        <button className="d-flex align-items-center btn btn-warning" title="Limpar">
+                            <FiAlertTriangle />&nbsp;Limpar
+                        </button>
+                    </div>
+                    <div className="col-md-1">
+                        <button className="d-flex align-items-center btn btn-danger" title="Deletar">
+                            <FiTrash />&nbsp;Deletar
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
     );
 };
