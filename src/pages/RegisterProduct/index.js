1e543a0dd4ed7e560d93cab69e312ba8fd2468fe
dev-viniciuss <dev.mvsc@gmail.com>
2020-04-21 18:16:43 -0300

    Validação dos campos da page de cadastro de produtos concluida

--- src/pages/RegisterProduct/index.js
+++ src/pages/RegisterProduct/index.js
@@ -1,40 +1,50 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import firebase from '../../config/firebase';
-import { FiCheck, FiCheckCircle, FiX } from 'react-icons/fi';
+import { FiCheck, FiCheckCircle, FiXCircle, FiX, FiAlertCircle } from 'react-icons/fi';
 
-export default function RegisterProduct(){
+export default function RegisterProduct() {
 
-    const [name, setName] = useState('');
-    const [sku, setSku] = useState('');
-    const [description, setDescription] = useState('');
+    const [name, setName] = useState();
+    const [sku, setSku] = useState();
+    const [description, setDescription] = useState();
     const [price, setPrice] = useState(0);
-    const [provider, setProvider] = useState('');
-    const [msgSucesso, setMsgSucess] = useState(false);
+    const [provider, setProvider] = useState();
+    const [msgType, setMsgType] = useState();
 
     const history = useHistory();
-    const db = firebase.firestore();
 
-    function handleSave(){
-        
-        db.collection('products').add({
-            name: name,
-            sku: sku,
-            description: description,
-            price: price,
-            provider: provider
-        }).then(() => {
-            setMsgSucess(true);
-            setTimeout(() => history.push('/query'), 3000);
-        }).catch( erro => {
-            alert('OPS!Algo deu errado')
-        } )
-
+    function handleSave() {
+        // eslint-disable-next-line
+        if (name, sku, description, provider === undefined) {
+            setMsgType('erro');
+        }
+        else if (price === 0) {
+            setMsgType('erro');
+        }
+        else {
+            firebase.firestore().db.collection('products').add({
+                name: name,
+                sku: sku,
+                description: description,
+                price: price,
+                provider: provider
+            }).then(() => {
+                setMsgType('sucess');
+                setTimeout(() => history.push('/query'), 3000);
+            }).catch(erro => {
+                setMsgType('erro');
+            })
+        }
     };
 
-    function handleCancel(){
-        alert('cancelado');
+    function handleCancel() {
+        setMsgType('cancel')
         setTimeout(() => history.push('/'), 3000);
+    };
+
+    function closeMsg() {
+        setMsgType();
     };
 
     return (
@@ -44,35 +54,47 @@ export default function RegisterProduct(){
             </div>
 
             <div className="card-body">
-                { msgSucesso && 
+                {msgType === 'sucess' &&
                     <div className="alert alert-dismissible alert-success">
-                        <button type="button" className="close" data-dismiss="alert">&times;</button>
-                        <strong><FiCheckCircle size="16"/> Cadastro concluido!</strong> Cadastro do produto realizado com sucesso!
+                        <button onClick={closeMsg} type="button" className="close" data-dismiss="alert">&times;</button>
+                        <strong><FiCheckCircle size="16" /> Wow!</strong> Cadastro do produto realizado com sucesso!
+                    </div>
+                }
+                {msgType === 'erro' &&
+                    <div class="alert alert-dismissible alert-danger">
+                        <button onClick={closeMsg} type="button" class="close" data-dismiss="alert">&times;</button>
+                        <strong><FiXCircle size="16" /> Ops!</strong> Verifique se todos os campos estão preenchidos corretamente!
+                    </div>
+                }
+                {msgType === 'cancel' &&
+                    <div class="alert alert-dismissible alert-warning">
+                        <button onClick={closeMsg} type="button" class="close" data-dismiss="alert">&times;</button>
+                        <strong><FiAlertCircle size="16" /> Vish!</strong> O cadastro desse produto foi cancelado!
                     </div>
                 }
 
                 <div className="row">
                     <div className="col-md-6">
-                       <div className="form-group">
-                           <label><strong>Nome: </strong></label>
-                           <input
-                           value={name}
-                           onChange={(e) => setName(e.target.value)} 
-                           type="text" 
-                           className="form-control"
-                           />
-                       </div>
+                        <div className="form-group">
+                            <label><strong>Nome: </strong></label>
+                            <input
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                type="text"
+                                className="form-control"
+                            />
+                        </div>
                     </div>
                     <div className="col-md-6">
                         <div className="form-group">
-                           <label><strong>SKU: </strong></label>
-                           <input
-                           value={sku}
-                           onChange={(e) => setSku(e.target.value)} 
-                           type="text" 
-                           className="form-control"
-                           />
-                       </div>
+                            <label><strong>SKU: </strong></label>
+                            <input
+                                value={sku}
+                                onChange={(e) => setSku(e.target.value)}
+                                type="text"
+                                className="form-control"
+                            />
+                        </div>
                     </div>
                 </div>
 
@@ -81,11 +103,11 @@ export default function RegisterProduct(){
                 <div className="row">
                     <div className="col-md-12">
                         <label><strong>Descrição: </strong></label>
-                        <textarea 
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        className="form-control" 
-                        /> 
+                        <textarea
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            className="form-control"
+                        />
                     </div>
                 </div>
 
@@ -93,26 +115,26 @@ export default function RegisterProduct(){
 
                 <div className="row">
                     <div className="col-md-6">
-                       <div className="form-group">
-                           <label><strong>Preço: </strong></label>
-                           <input 
-                           value={price}
-                           onChange={(e) => setPrice(e.target.value)}
-                           type="text" 
-                           className="form-control"
-                           />
-                       </div>
+                        <div className="form-group">
+                            <label><strong>Preço: </strong></label>
+                            <input
+                                value={price}
+                                onChange={(e) => setPrice(e.target.value)}
+                                type="text"
+                                className="form-control"
+                            />
+                        </div>
                     </div>
                     <div className="col-md-6">
                         <div className="form-group">
-                           <label><strong>Fornecedor: </strong></label>
-                           <input 
-                           value={provider}
-                           onChange={(e) => setProvider(e.target.value)}
-                           type="text" 
-                           className="form-control"
-                           />
-                       </div>
+                            <label><strong>Fornecedor: </strong></label>
+                            <input
+                                value={provider}
+                                onChange={(e) => setProvider(e.target.value)}
+                                type="text"
+                                className="form-control"
+                            />
+                        </div>
                     </div>
                 </div>
 
